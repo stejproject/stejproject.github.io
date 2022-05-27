b8d5cedb4e98abce562f05961b27e8baa6e6f410
stejproject <106367163+stejproject@users.noreply.github.com>
2022-05-27 21:58:16 +0700

    Update anti.js

--- js/anti.js
+++ js/anti.js
@@ -12,20 +12,3 @@ if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
 return false;
 }
 }
-
-var isNS = (navigator.appName == &quot;Netscape&quot;) ? 1 : 0;
-if(navigator.appName == &quot;Netscape&quot;) document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
-function mischandler(){
-return false;
-}
-function mousehandler(e){
-var myevent = (isNS) ? e : event;
-var eventbutton = (isNS) ? myevent.which : myevent.button;
-(eventbutton==2)||(eventbutton==3)) {
- alert("copyThieft");
- return false;
- }
-}
-document.oncontextmenu = mischandler;
-document.onmousedown = mousehandler;
-document.onmouseup = mousehandler;
