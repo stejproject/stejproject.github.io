b9642e1a071ae25e35d1e2a49eae28f48fcc1637
stejproject <belajarbroedu@gmail.com>
2022-10-07 13:21:12 +0700

    delete spacegun.js

--- js/spacegun.js
+++
@@ -1,7 +0,0 @@
-var message = "**This page is no longer active**";
-
-function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
-
-if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
-
-document.onmousedown = rtclickcheck;
